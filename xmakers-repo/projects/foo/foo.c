b6380a3ab461f8814eb9d5c27dc7d1fb209ee8fc
A2va <49582555+A2va@users.noreply.github.com>
2025-03-12 12:00:11 +0100

    Remove xmakers-repo

--- xmakers-repo/projects/foo/foo.c
+++
@@ -1,5 +0,0 @@
-#include "foo/foo.h"    
-
-int foo() {
-    return 123;    
-}
--- xmakers-repo/projects/foo/xmake.lua
+++
@@ -1,9 +0,0 @@
-target("foo")
-    set_kind("$(kind)")
-    add_files("foo.c")
-    add_headerfiles("foo/foo.h", {prefixdir = "foo"})
-
-    add_defines("FOO_BUILD")
-    if is_kind("static") then
-        add_defines("FOO_STATIC", {public = true})
-    end
